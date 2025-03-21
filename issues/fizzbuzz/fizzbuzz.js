ee8e0cf0b75966fd383235bad2e5cc0367ec0fa8
Kazuki <okazuki58@gmail.com>
2025-03-21 22:21:08 +0900

    feat: fizzbuzzの問題に解答

--- issues/fizzbuzz/fizzbuzz.js
+++ issues/fizzbuzz/fizzbuzz.js
@@ -1,5 +1,12 @@
 function fizzBuzz(n) {
-  //ここに実装内容を書いてください
+  const result = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) result.push("FizzBuzz");
+    else if (i % 3 === 0) result.push("Fizz");
+    else if (i % 5 === 0) result.push("Buzz");
+    else result.push(i.toString());
+  }
+  return result;
 }
 
 module.exports = fizzBuzz;
